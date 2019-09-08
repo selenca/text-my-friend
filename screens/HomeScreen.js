bc7f7bf7119745c28670286c7e43df4c92cf4029
selen <selen.caca@gmail.com>
2019-09-08 12:42:27 +0800

    Initial setup of reminder app
    Create Reminder button which trigger alert (WIP)
    History of Reminders which trigger alert (WIP)

---
+++ screens/HomeScreen.js
@@ -0,0 +1,203 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { MonoText } from '../components/StyledText';
+import { Card, List, ListItem, Button, Icon } from 'react-native-elements';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <View style={styles.welcomeContainer}>
+          <Image
+            source={
+              __DEV__
+                ? require('../assets/images/robot-dev.png')
+                : require('../assets/images/robot-prod.png')
+            }
+            style={styles.welcomeImage}
+          />
+        </View>
+
+        <View style={styles.getStartedContainer}>
+
+          <Text style={styles.getStartedText}>
+            Get started by clicking {"\n"}
+          </Text>
+
+          <Button
+            onPress={createReminder}
+            title="Create Reminder"
+            color="black"
+          />  
+
+          <Text style={styles.getStartedText}>
+            {"\n"} Below you will see the reminders sorted by date
+          </Text>
+
+          <RenderReminderList />
+
+        </View>
+
+      </ScrollView>
+
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+function createReminder() {
+  alert("Dummy create reminder");
+}
+
+// name on top, when and what in  the bottom
+const reminderList = [
+  {
+    name: 'Yesim',
+    subtitle: 'When | What',
+    reminder_type: 'mail'
+  },
+  {
+    name: 'Ana',
+    subtitle: 'When | What',
+    reminder_type: 'phone'
+  }
+  // {
+  //   name: 'Monja',
+  //   subtitle: 'When | What',
+  //   reminder_type: 'slack'
+  // }
+];
+ 
+
+function RenderReminderList() {
+  return(  
+    <View style={styles.reminderContainer}>
+      {
+        reminderList.map((l, i) => (
+          <ListItem
+            key={i}
+            leftAvatar=<Icon name={l.reminder_type} />
+            title={l.name}
+            subtitle={l.subtitle}
+            bottomDivider
+            onPress={() => openReminder()}
+          />
+        ))
+      }
+    </View>
+  );
+}
+
+function openReminder() {
+  alert("Show | Modify");
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  reminderContainer: {
+    padding: 0,
+    width: "100%",
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+
+});
